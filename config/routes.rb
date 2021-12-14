d0de7804e95da76342095f1842a558fc1fcee33c
EC2 Default User <93399430+kawa789@users.noreply.github.com>
2021-12-14 18:43:46 +0000

    [Update]いいね機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
-  resources :books
-  resource :favorites, only: [:create, :destroy]
+  resources :books do
+   resource :favorites, only: [:create, :destroy]
+  end
   root 'homes#top'
   get 'home/about' => 'homes#about'
 end
