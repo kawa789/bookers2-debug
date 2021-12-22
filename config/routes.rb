32745cf7babc545a70e543fd2f59de884a2e094b
EC2 Default User <93399430+kawa789@users.noreply.github.com>
2021-12-22 06:55:45 +0000

    [Add]フォロー機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+  #get 'relationships/followings'
+  #get 'relationships/followers'
   devise_for :users
   resources :users,only: [:show,:index,:edit,:update]
   resources :books do
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
   end
+
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
+
   root 'homes#top'
   get 'home/about' => 'homes#about'
 end
