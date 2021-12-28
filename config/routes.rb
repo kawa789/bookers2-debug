6718a61c3ee18de1af82643616fb3d589e990275
EC2 Default User <93399430+kawa789@users.noreply.github.com>
2021-12-28 09:24:11 +0000

    [Add]フォロー　ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,11 @@ Rails.application.routes.draw do
   #get 'relationships/followings'
   #get 'relationships/followers'
 
-  resources :relationships, only: [:create, :destroy]
 
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+  resource :relationships, only: [:create, :destroy]
+  end
   resources :books do
    resource :favorites, only: [:create, :destroy]
    resources :book_comments, only: [:create, :destroy]
