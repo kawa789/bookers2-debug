678d938f26041bddb2216d8210e6c091041b84d3
EC2 Default User <93399430+kawa789@users.noreply.github.com>
2022-01-15 17:53:25 +0000

    [Add]コメント、いいね機能非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,15 +2,23 @@ class BookCommentsController < ApplicationController
 
 def create
   @book = Book.find(params[:book_id])
-  @comment = current_user.book_comments.new(book_comment_params)
-  @comment.book_id = @book.id
-  @comment.save
-  redirect_back(fallback_location: root_path)
+  @book_comment = BookComment.new(book_comment_params)
+  @book_comment.book_id = @book.id
+  @book_comment.user_id = current_user.id
+  @book_comment.save
+  #@book = Book.find(params[:book_id])
+  #@comment = current_user.book_comments.new(book_comment_params)
+  #@comment.book_id = @book.id
+  #@comment.save
+  #redirect_back(fallback_location: root_path)
 end
 
 def destroy
-  BookComment.find_by(id: params[:id]).destroy
-  redirect_back(fallback_location: root_path)
+  #BookComment.find_by(id: params[:id]).destroy
+  @book = Book.find(params[:book_id])
+  book_comment = @book.book_comments.find(params[:id])
+  book_comment.destroy
+  #redirect_back(fallback_location: root_path)
 end
 
 private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,18 @@
 class FavoritesController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: @book.id)
+    #favorite = current_user.favorites.new(book_id: @book.id)
+    favorite = @book.favorites.new(user_id: current_user.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path) #非同期通信
   end
 
   def destroy
     @book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: @book.id)
+    favorite = @book.favorites.find_by(user_id: current_user.id)
+    #favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
+    #redirect_back(fallback_location: root_path) #非同期通信
   end
 
 end
